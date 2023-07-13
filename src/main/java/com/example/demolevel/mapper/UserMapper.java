daed571f6b5bae5c18c12af2827eefb719510c74
Rwear <20172333127@m.scnu.edu.cn>
2023-07-12 23:44:07 -0400

    start the root of the project

---
+++ src/main/java/com/example/demolevel/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.demolevel.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demolevel.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/example/demolevel/DemoLevelApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.demolevel;
+
+import com.example.demolevel.mapper.UserMapper;
+import com.example.demolevel.model.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class DemoLevelApplicationTests {
+
+    @Resource
+    private UserMapper userMapper;
+    @Test
+    void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+}
